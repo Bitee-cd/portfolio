e7cacf509d6d65356b7248b292cecf44c2d4b10e
Bitee-cd <65104898+Bitee-cd@users.noreply.github.com>
2022-03-24 00:17:22 +0100

    web vitals

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -6,11 +6,10 @@ import './StyledComponents/Navigation.css'
 function Navigation() {
     return (
         <div>
-            <Navbar className="shadow"  bg="light" expand="lg">
+            <Navbar className="shadow"  collapseOnSelect expand="md">
                 <Container>
                     <Navbar.Brand href="#">BIT33</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
+                    <Navbar.Collapse id="offcanvasNavbar">
                     <Nav className="ms-auto">
                             <Link className="nav-link" to="/">Home</Link>
                             <Link  className="nav-link" to="about">About</Link>
@@ -19,6 +18,27 @@ function Navigation() {
                             <Link  className="nav-link" to="contact">Contact</Link>
                     </Nav>
                     </Navbar.Collapse>
+                    <Navbar.Toggle aria-controls="offcanvasNavbar" />
+                    <Navbar.Offcanvas
+                    id="offcanvasNavbar responsive-navbar-nav"
+                    aria-labelledby="offcanvasNavbarLabel"
+                    placement="start"
+                    >
+                    <Offcanvas.Header closeButton>
+                        <Offcanvas.Title id="offcanvasNavbarLabel">BIT33</Offcanvas.Title>
+                    </Offcanvas.Header>
+                    <Offcanvas.Body className="text-center">
+                            <Nav className="justify-content-end flex-grow-1 pe-3 ">
+                            <Nav.Link href="/" className="text-black">Home</Nav.Link>
+                            <Nav.Link href="about" className="text-black">About</Nav.Link>
+                            <Nav.Link href="skills" className="text-black">Skills</Nav.Link>
+                            <Nav.Link href="work" className="text-black">Works</Nav.Link>
+                            <Nav.Link href="contact" className="text-black">Contact</Nav.Link>
+                            
+                            </Nav>
+                        </Offcanvas.Body>
+                    </Navbar.Offcanvas>
+                   
                 </Container>
             </Navbar>
         </div>
